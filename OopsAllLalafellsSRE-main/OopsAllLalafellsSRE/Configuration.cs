516722b27b3b5e3b79484405839376ac83d9dec3
K0shachiya_mýãțA <88767403+K0shachiyamyatA@users.noreply.github.com>
2024-08-13 23:57:14 +0400

    Add files via upload

---
+++ OopsAllLalafellsSRE-main/OopsAllLalafellsSRE/Configuration.cs
@@ -0,0 +1,31 @@
+using Dalamud.Configuration;
+using Dalamud.Plugin;
+using System;
+using static OopsAllLalafellsSRE.Utils.Constant;
+
+namespace OopsAllLalafellsSRE
+{
+    [Serializable]
+    internal class Configuration : IPluginConfiguration
+    {
+        public int Version { get; set; } = 0;
+        public Race SelectedRace { get; set; } = Race.LALAFELL;
+        public bool enabled { get; set; } = false;
+        public bool stayOn { get; set; } = false;
+        public bool nameHQ { get; set; } = true;
+
+        // the below exist just to make saving less cumbersome
+        [NonSerialized]
+        private IDalamudPluginInterface? pluginInterface;
+
+        public void Initialize(IDalamudPluginInterface pluginInterface)
+        {
+            this.pluginInterface = pluginInterface;
+        }
+
+        public void Save()
+        {
+            pluginInterface!.SavePluginConfig(this);
+        }
+    }
+}
